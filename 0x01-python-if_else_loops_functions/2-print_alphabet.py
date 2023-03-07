3c706cd2b2a97212502a74732f7c81a5c7350fed
John Agbaje <agbajeakin84@gmail.com>
2023-03-07 10:28:03 -0800

    gkf

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,3 @@ for i in range(97, 123):
     tableAsciiToAlphabet = chr(i)
     asciiToAlpha = "" + tableAsciiToAlphabet
     print("{}".format(asciiToAlpha), end='')
-
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,3 @@ for alpha in range(97, 123):
     if alpha == 101 or alpha == 113:
         continue
     print("{}".format(chr(alpha)), end="")
-
